359e6fefc01f115b6c57890b0e4ef83b79f94fa2
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2021-11-02 22:54:58 -0300

    Candidatos - Pronto

--- load_file_pandas.py
+++ load_file_pandas.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-# PATH_IN = r'C:\Users\jean_\Downloads\votacao_candidato_munzona_2018 (4)\votacao_candidato_munzona_2018_AP.csv'
-# PATH_IN = r'C:\Users\jean_\Downloads\votacao_candidato_munzona_2018 (4)\votacao_candidato_munzona_2018_RS.csv'
-# PATH_IN = r'C:\Users\jean_\Downloads\votacao_candidato_munzona_2018 (4)\votacao_candidato_munzona_2018_SP.csv'
+COLUMN_LIST = ["SG_UF", "SG_UE", "NM_UE", "CD_MUNICIPIO", "NM_MUNICIPIO", "NR_ZONA", "DS_CARGO", "NM_CANDIDATO", "NM_URNA_CANDIDATO", "TP_AGREMIACAO", "NR_PARTIDO", "SG_PARTIDO", "NM_COLIGACAO", "DS_COMPOSICAO_COLIGACAO", "DS_SIT_TOT_TURNO", "QT_VOTOS_NOMINAIS"]
+
 PATH_IN = r'C:\Users\jean_\Downloads\votacao_candidato_munzona_2018 (4)\votacao_candidato_munzona_2018_BRASIL.csv'
 data = pd.read_csv(PATH_IN, encoding = "ISO-8859-1", sep=";")
-print(data.head())
-print(PATH_IN)
 
-# salvar parte do Eduardo Bolsonaro
-data[data["NM_URNA_CANDIDATO"]=="EDUARDO BOLSONARO"].to_csv("Eduardo Bolsonaro.csv", sep=';', encoding='ISO-8859-1')
-# salvar parte da Joice Hasselmann
-data[data["NM_URNA_CANDIDATO"]=="JOICE HASSELMANN"].to_csv("choice.csv", sep=';', encoding='ISO-8859-1')
-# salvar parte do Kim
-data[data["NM_URNA_CANDIDATO"]=="KIM KATAGUIRI"].to_csv("kim basinger.csv", sep=';', encoding='ISO-8859-1')
+def save_csv(candidato, df=data):
+    df[df["NM_URNA_CANDIDATO"]==candidato][COLUMN_LIST].to_csv(candidato + ".csv", sep=';', encoding='ISO-8859-1')
+
+save_csv("EDUARDO BOLSONARO")
+save_csv("JOICE HASSELMANN")
+save_csv("KIM KATAGUIRI")
+
+data[['SG_PARTIDO', 'NM_PARTIDO', 'NM_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO']].drop_duplicates(['SG_PARTIDO', 'NM_PARTIDO', 'NM_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO'],keep= 'last').to_csv("partidos e coligações.csv", sep=';', encoding='ISO-8859-1')
+
+data.groupby(['NM_UE', 'SG_PARTIDO', 'NM_PARTIDO', 'NM_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO']).QT_VOTOS_NOMINAIS.agg('sum')
+data[data['DS_CARGO']=='Deputado Federal'].groupby(['NM_UE', 'SG_PARTIDO', 'NM_PARTIDO', 'NM_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO'], as_index=False).QT_VOTOS_NOMINAIS.agg('sum').to_csv("group by partido dep fed.csv", sep=';', encoding='ISO-8859-1')
+data[data['DS_CARGO']=='Deputado Estadual'].groupby(['NM_UE', 'SG_PARTIDO', 'NM_PARTIDO', 'NM_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO'], as_index=False).QT_VOTOS_NOMINAIS.agg('sum').to_csv("group by partido dep fed.csv", sep=';', encoding='ISO-8859-1')
